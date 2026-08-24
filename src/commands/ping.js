10ac7c997bfe1583f041376e62b68523035aa60e
hesjstlucas <heherohuh@gmail.com>
2026-08-24 12:29:50 -0400

    E

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,11 +1,12 @@
-import {SlashCommandBuilder,
-    PermissionFlagsBits
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
 } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('Check the bot\'s latency');
+    .setName("ping")
+    .setDescription("Check the bot's latency.");
 
-  async function execute(interaction) {
-  await interaction.reply({ content: `Pong! Latency is ${Date.now() - interaction.createdTimestamp}ms.`, ephemeral: true });
-  }
+export async function execute(interaction) {
+    await interaction.reply("Pong!");
+}
