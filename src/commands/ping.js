75b84c4306da2b37136399d01198ecb12fbb04a9
hesjstlucas <heherohuh@gmail.com>
2026-08-24 11:52:39 -0400

    E

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -7,7 +7,5 @@ export const data = new SlashCommandBuilder()
   .setDescription('Check the bot\'s latency');
 
   async function execute(interaction) {
-    const sent = await interaction.reply({content: 'Pinging...', fetchReply: true});
-    const latency = sent.createdTimestamp - interaction.createdTimestamp;
-    await interaction.editReply(`Pong! Latency is ${latency}ms.`);
+  await interaction.reply({ content: `Pong! Latency is ${Date.now() - interaction.createdTimestamp}ms.`, ephemeral: true });
   }
